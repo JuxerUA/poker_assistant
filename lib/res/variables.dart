874d6aa5b7c570af4900ee4943c8258e1a03b0b8
roman.shashkov.cr@gmail.com <cleveroad89>
2022-08-10 08:56:40 +0300

    added variables.dart

---
+++ lib/res/variables.dart
@@ -0,0 +1,14 @@
+/// Variables that set the settings at the start of the game (total cash, players count)
+class PokerStartVariables {
+  // TODO
+}
+
+/// Variables that change during the game (game time, players count)
+class PokerInGameVariables {
+  // TODO
+}
+
+/// Variables responsible for application settings (blinds or bet)
+class Settings {
+  // TODO
+}
